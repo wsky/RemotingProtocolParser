40fcb7804bcfc41dd64fe4f36048d256a43eae70
wsky <wskyhx@gmail.com>
2013-03-04 15:45:13 +0800

    add java test file

---
+++ java/src/test/java/remoting/protocol/tcp/TcpProtocolTest.java
@@ -0,0 +1,15 @@
+package remoting.protocol.tcp;
+
+import org.junit.Test;
+
+public class TcpProtocolTest {
+	@Test
+	public void WriteRequestAndReadResponse() {
+
+	}
+
+	@Test
+	public void ReadRequestAndWriteResponse() {
+
+	}
+}
